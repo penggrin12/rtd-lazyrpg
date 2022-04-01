a30eec687dbeb55e373724328d555cbdd894d021
penggrin12 <76795163+penggrin12@users.noreply.github.com>
2022-04-01 14:09:51 +0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'LazyRPG'
+copyright = '2022, Penggrin'
+author = 'Penggrin'
 
 release = '0.1'
 version = '0.1.0'
